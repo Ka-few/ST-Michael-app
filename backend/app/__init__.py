779e4d36d91f1bb81c2be7ffb5e26aeefb3ed8fd
ka-few <njorovista@gmail.com>
2025-12-19 16:20:19 +0300

    install CORS dependency

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS 
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    
+    CORS(app)
 
     # Ensure instance folder exists
     os.makedirs(app.instance_path, exist_ok=True)
